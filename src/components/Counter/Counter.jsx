7ba131c96d76bf6b2d2c55fbcc9d06a39a445809
SSSerg05 <ssserg05@gmail.com>
2023-06-21 15:59:57 +0300

    fix

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -3,15 +3,25 @@ import PropTypes from 'prop-types'; // ES6
 import { Button } from './Counter.styled';
 
 export class Counter extends Component {
+
   static defaultProps = {};
 
   static propTypes = {};
+
+  incrementGood = (event) => { 
+    console.log("click Good");
+    console.log(this);
+    // in async code need save event in other value
+    const { target } = event;
+    console.log(target);
+  }
+
 
   render() {
     return (
       <div>
         <div>
-          <Button onClick={() => console.log("click Good")}>Good</Button>
+          <Button onClick={this.incrementGood}>Good</Button>
           <Button onClick={() => console.log("click Netral")}>Neitral</Button>
           <Button onClick={() => console.log("click Negative")}>Negative</Button>
         </div>
