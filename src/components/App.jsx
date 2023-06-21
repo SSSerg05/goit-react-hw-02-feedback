925898ccfcb1301b17ecfb8fcb2c6c78dbd0020a
SSSerg05 <ssserg05@gmail.com>
2023-06-21 13:19:52 +0300

    add template app

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        margin: '0 auto',
+        // height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'nowrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      its Work!
+
+    </div>
+  );
+};
